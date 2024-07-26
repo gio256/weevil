eddcf41630ca8f7974466c2866e5f694a9acba25
gio256 <102917377+gio256@users.noreply.github.com>
2024-07-26 12:10:18 -0600

    Add test_deadlock

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,57 @@
 #[cfg(test)]
-mod tests {
-    use loom::sync::Arc;
+mod loom {
+    use std::rc::Rc;
+
+    use loom::sync::{Arc, Mutex};
     use loom::sync::atomic::AtomicUsize;
-    use loom::sync::atomic::Ordering::{Acquire, Release, Relaxed};
+    use loom::sync::atomic::Ordering;
     use loom::thread;
 
     #[test]
     #[should_panic]
-    fn buggy_concurrent_inc() {
+    fn test_deadlock() {
         loom::model(|| {
-            let num = Arc::new(AtomicUsize::new(0));
+            let a = Rc::new(Mutex::new(1));
+            let b = Rc::new(Mutex::new(2));
 
-            let ths: Vec<_> = (0..2)
+            let t0 = {
+                let a = a.clone();
+                let b = b.clone();
+
+                thread::spawn(move || {
+                    let a = a.lock().unwrap();
+                    let b = b.lock().unwrap();
+                    assert_eq!(*a + *b, 3);
+                })
+            };
+            let t1 = thread::spawn(move || {
+                    let b = b.lock().unwrap();
+                    let a = a.lock().unwrap();
+                    assert_eq!(*a + *b, 3);
+            });
+            t0.join().unwrap();
+            t1.join().unwrap();
+        });
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_inc() {
+        loom::model(|| {
+            let val = Arc::new(AtomicUsize::new(0));
+            let threads: Vec<_> = (0..2)
                 .map(|_| {
-                    let num = num.clone();
+                    let val = val.clone();
                     thread::spawn(move || {
-                        let curr = num.load(Acquire);
-                        num.store(curr + 1, Release);
+                        let cur = val.load(Ordering::Acquire);
+                        val.store(cur + 1, Ordering::Release);
                     })
                 })
                 .collect();
-
-            for th in ths {
-                th.join().unwrap();
+            for t in threads {
+                t.join().unwrap();
             }
-
-            assert_eq!(2, num.load(Relaxed));
+            assert_eq!(val.load(Ordering::Relaxed), 2);
         });
     }
 }
