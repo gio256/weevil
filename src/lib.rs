11340459eaffa92bcc5fd4a7f6ba223b56263162
gio256 <102917377+gio256@users.noreply.github.com>
2024-07-27 09:24:14 -0600

    Add test_relaxed

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 mod loom {
     use std::rc::Rc;
 
-    use loom::sync::{Arc, Mutex};
     use loom::sync::atomic::AtomicUsize;
-    use loom::sync::atomic::Ordering;
+    use loom::sync::atomic::Ordering::{Acquire, Release, Relaxed};
+    use loom::sync::{Arc, Mutex};
     use loom::thread;
 
     #[test]
@@ -17,7 +17,6 @@ mod loom {
             let t0 = {
                 let a = a.clone();
                 let b = b.clone();
-
                 thread::spawn(move || {
                     let a = a.lock().unwrap();
                     let b = b.lock().unwrap();
@@ -25,9 +24,9 @@ mod loom {
                 })
             };
             let t1 = thread::spawn(move || {
-                    let b = b.lock().unwrap();
-                    let a = a.lock().unwrap();
-                    assert_eq!(*a + *b, 3);
+                let b = b.lock().unwrap();
+                let a = a.lock().unwrap();
+                assert_eq!(*a + *b, 3);
             });
             t0.join().unwrap();
             t1.join().unwrap();
@@ -43,15 +42,42 @@ mod loom {
                 .map(|_| {
                     let val = val.clone();
                     thread::spawn(move || {
-                        let cur = val.load(Ordering::Acquire);
-                        val.store(cur + 1, Ordering::Release);
+                        let cur = val.load(Acquire);
+                        val.store(cur + 1, Release);
                     })
                 })
                 .collect();
             for t in threads {
                 t.join().unwrap();
             }
-            assert_eq!(val.load(Ordering::Relaxed), 2);
+            assert_eq!(val.load(Relaxed), 2);
+        });
+    }
+
+    #[test]
+    #[ignore]
+    #[should_panic]
+    fn test_relaxed() {
+        loom::model(|| {
+            let a = Arc::new(AtomicUsize::new(0));
+            let b = Arc::new(AtomicUsize::new(0));
+            let t0 = {
+                let a = a.clone();
+                let b = b.clone();
+                thread::spawn(move || {
+                    let res = b.load(Relaxed);
+                    a.store(1, Relaxed);
+                    res
+                })
+            };
+            let t1 = thread::spawn(move || {
+                let res = a.load(Relaxed);
+                b.store(1, Relaxed);
+                res
+            });
+            let res0 = t0.join().unwrap();
+            let res1 = t1.join().unwrap();
+            assert!(!(res0 == 1 && res1 == 1));
         });
     }
 }
